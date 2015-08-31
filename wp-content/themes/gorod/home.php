0a9017d678ceb5761a66a00f3c5a2189165bea7a
TimurMG <k-tim91@mail.ru>
2015-08-31 18:03:25 +0400

    Testiruem

--- wp-content/themes/gorod/home.php
+++ wp-content/themes/gorod/home.php
@@ -84,10 +84,6 @@ get_header();
 
 <div class="tab_container">
 <div id="tab1" class="tab_content">
-
-Git project
-dfgdfgdfg
-
 <?php 
 $posts = get_posts(array('post_type' => 'news', 'posts_per_page' => 5, 'tax_query' => array(array('taxonomy' => 'ncategory', 'field' => 'slug', 'terms' => 'lenta')))); 
 // var_dump($cat_posts);
